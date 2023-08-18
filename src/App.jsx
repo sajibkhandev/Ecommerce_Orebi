cf8d0db3000d2623a0df0f811a78329f6c554c2e
Sajib Khan <sajibkhan.mern@gmail.com>
2023-08-18 19:22:27 +0600

    tailwind add

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function App() {
   return (
    <>
-   <h1>This Ecommerce</h1>
-   <p>This is Ecommerce</p>
+   <h1 className='text-red-500 text-5xl'>This Ecommerce</h1>
+   <p className='text-base text-blue-500'>This is Ecommerce</p>
     </>
   )
 }
